323ef95154e9c58d3400be241a48e7b2923f0b19
Pedro Maschio <pedrotorres_maschio@hotmail.com>
2022-07-09 19:15:54 -0300

    Starting assignement 2

--- Trabalho 1/ex1.hs
+++ Trabalho 1/ex1.hs
@@ -8,9 +8,9 @@ import Data.List (sortBy)
 maior4 :: Int -> Int -> Int -> Int -> Int 
 
 maior4 a b c d 
-    |   a > b && a > c && a > d = a
-    |   b > a && b > c && b > d = b
-    |   c > a && c > b && c > d = c
+    |   a >= b && a >= c && a >= d = a
+    |   b >= a && b >= c && b >= d = b
+    |   c >= a && c >= b && c >= d = c
     | otherwise = d
 
 
